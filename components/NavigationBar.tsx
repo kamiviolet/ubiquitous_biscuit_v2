791bd0b1b8183e0062c4b337fdafe2195b67445c
kamiviolet <kamiviolet@gmail.com>
2023-08-05 20:05:01 +0100

    initial setup

---
+++ components/NavigationBar.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import LogoutButton from '../components/LogoutButton'
+import { User } from '@/types/types'
+
+export default function NavigationBar({user}:{user: User|null}) {
+    return (
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
+          <div />
+          <div>
+            {user ? (
+              <div className="flex items-center gap-4">
+                Hey, {user.email}!
+                <LogoutButton />
+              </div>
+            ) : (
+              <Link
+                href="/login"
+                className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav>
+    )
+}
